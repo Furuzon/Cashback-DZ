9d40f9ce14be219e261e307002a9550c66d0fad3
furuzon <furuzon@gmailmadaliev.com>
2020-01-12 19:48:11 -0800

    Исправленое 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 
-const REGULAR_CATEGORY = 0.01;
-const INCREASED_CATEGORY = 0.05;
-const SPECIAL_CATEGORY = 0.3;
+const REGULAR_CATEGORY = 'regular';
+const INCREASED_CATEGORY = 'increased';
+const SPECIAL_CATEGORY = 'special';
 
 const purchases = [
     {
-        amount: 10000,
+        amount: 1000,
         category: REGULAR_CATEGORY
     },
     {
@@ -17,19 +17,22 @@ const purchases = [
         category: REGULAR_CATEGORY,
     },
     {
-        amount: 1000,
-        category: SPECIAL_CATEGORY,
-    },
-    {
-        amount: 28,
+        amount: 10000,
         category: SPECIAL_CATEGORY,
     },
 ];
 let cashback = 0;
 
 for(let purchase of purchases){
-    cashback += purchase.amount*purchase.category;
+    if(purchase.category === 'regular'){
+        cashback += purchase.amount*0.01;
+    }else if(purchase.category === 'increased'){
+        cashback += purchase.amount*0.05;
+    }else if(purchase.category === 'special'){
+        cashback += purchase.amount*0.3;
+    }
+    
 }
 cashback = cashback > 3000 ? 3000:cashback;
 
-console.log(cashback);
+console.log(cashback)
